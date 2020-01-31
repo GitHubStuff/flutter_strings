498cb416c9c32d79a63569be56205c5164b132e3
Steven Smith <github@icodeforyou.com>
2020-01-31 11:46:15 -0500

    removed unneeded pad option

--- lib/src/string_helpers.dart
+++ lib/src/string_helpers.dart
@@ -3,13 +3,3 @@
 String capitalize(String s) => (s == null) ? null : s[0].toUpperCase() + s.substring(1);
 
 String lowercase(String s) => (s == null) ? null : s[0].toLowerCase() + s.substring(1);
-
-String pad(int size, {String initialValue = '', String using = ' '}) {
-  assert(initialValue != null);
-  assert(using != null && using.length > 0);
-  assert(size > 0);
-  while (initialValue.length < size) {
-    initialValue += using;
-  }
-  return initialValue.substring(0, size);
-}
--- test/flutter_strings_test.dart
+++ test/flutter_strings_test.dart
@@ -1,10 +1,5 @@
-import 'package:flutter_strings/flutter_strings.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {
-    String val = 'duck-';
-    val = pad(15, initialValue: val, using: 'quack');
-    expect(val, 'duck-quackquack');
-  });
+  test('adds one to input values', () {});
 }
