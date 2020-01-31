34929c8f02069bd910486e3537afc40d26804404
Steven Smith <github@icodeforyou.com>
2020-01-31 18:21:09 -0500

    new feature to get a flutter style file name from a string

--- lib/src/string_helpers.dart
+++ lib/src/string_helpers.dart
@@ -3,3 +3,20 @@
 String capitalize(String s) => (s == null) ? null : s[0].toUpperCase() + s.substring(1);
 
 String lowercase(String s) => (s == null) ? null : s[0].toLowerCase() + s.substring(1);
+
+String flutterFilenameStyle({String using}) {
+  if (using == null || using.isEmpty) return using;
+  bool wasUppercase = true;
+  List<String> result = List();
+  final List<String> letters = using.split('');
+  for (String character in letters) {
+    if (RegExp(r'^[A-Z]+$').hasMatch(character)) {
+      if (!wasUppercase) result.add('_');
+      wasUppercase = true;
+    } else {
+      wasUppercase = false;
+    }
+    result.add(character.toLowerCase());
+  }
+  return result.join();
+}
--- test/flutter_strings_test.dart
+++ test/flutter_strings_test.dart
@@ -1,5 +1,29 @@
+import 'package:flutter_strings/flutter_strings.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  test('adds one to input values', () {});
+  test('double first letters', () {
+    final source = 'FFancyTableNameForDB';
+    final answer = flutterFilenameStyle(using: source);
+    final compare = 'ffancy_table_name_for_db';
+    assert(compare == answer);
+  });
+  test('start with cap', () {
+    final source = 'FancyTableNameForDB';
+    final answer = flutterFilenameStyle(using: source);
+    final compare = 'fancy_table_name_for_db';
+    assert(compare == answer);
+  });
+  test('start with lowercase', () {
+    final source = 'fancyTableNameForDB';
+    final answer = flutterFilenameStyle(using: source);
+    final compare = 'fancy_table_name_for_db';
+    assert(compare == answer);
+  });
+  test('start more than two consecutive cap letters', () {
+    final source = 'fancyTableNameForDBA';
+    final answer = flutterFilenameStyle(using: source);
+    final compare = 'fancy_table_name_for_dba';
+    assert(compare == answer);
+  });
 }
